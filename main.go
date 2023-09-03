ff25b78db90f6e4c9507e407660e6ceb7c759c42
black-crowned-night-heron <mobys.nycticorax@gmail.com>
2023-09-03 19:57:19 -0400

    Get rid of globals
    I have created a new type `TaskList` which is a struct containing a slice of `Task` structs. All the previous methods which were operating on the global `Tasks` slice are now methods on the `TaskList` struct and operate on the `Tasks` field inside the `TaskList` structure. In fact, the `main.go` file does not contain any global variables anymore: the filename is passed directly as an argument to the `loadTasksFromFile` and `saveTasksToFile` methods and a new `TaskList` instance is created directly in the `main` function.
    
    Resolves #23

--- main.go
+++ main.go
@@ -1,184 +1,140 @@
+```
 package main
 
 import (
-	"bufio"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"strconv"
-	"strings"
-	"time"
+    "bufio"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "os"
+    "strconv"
+    "strings"
+    "time"
 )
 
 type Task struct {
-	Description string        `json:"description"`
-	Finished    bool          `json:"finished"`
-	WorkTime    time.Duration `json:"work_time"`
+    Description string        `json:"description"`
+    Finished    bool          `json:"finished"`
+    WorkTime    time.Duration `json:"work_time"`
 }
 
-var tasks []Task
-
-func printTasks() {
-	fmt.Println("Tasks:")
-
-	printTaskList(tasks)
-
-	totalTime := time.Duration(0)
-	for _, task := range tasks {
-		totalTime += task.WorkTime
-	}
-	fmt.Printf("\nTotal time spent on all tasks: %v\n", totalTime.Truncate(time.Second))
+type TaskList struct {
+    Tasks []Task
 }
 
-func printTaskList(taskList []Task) {
-	for i, task := range taskList {
-		if !task.Finished {
-			continue
-		}
-		printTask(task, i+1, "Finished")
-	}
-	fmt.Println("------------------------------------------")
-	for i, task := range taskList {
-		if task.Finished {
-			continue
-		}
-		printTask(task, i+1, "Unfinished")
-	}
+func (tl *TaskList) printTasks() {
+    fmt.Println("Tasks:")
+
+    tl.printTaskList()
+
+    totalTime := time.Duration(0)
+    for _, task := range tl.Tasks {
+        totalTime += task.WorkTime
+    }
+    fmt.Printf("\nTotal time spent on all tasks: %v\n", totalTime.Truncate(time.Second))
 }
 
-func printTask(task Task, number int, status string) {
-	fmt.Printf("%-5d %-10s %-10v %s\n", number, status, task.WorkTime.Truncate(time.Second), task.Description)
+func (tl *TaskList) printTaskList() {
+    for i, task := range tl.Tasks {
+        if !task.Finished {
+            continue
+        }
+        tl.printTask(task, i+1, "Finished")
+    }
+    fmt.Println("------------------------------------------")
+    for i, task := range tl.Tasks {
+        if task.Finished {
+            continue
+        }
+        tl.printTask(task, i+1, "Unfinished")
+    }
 }
 
-func addTask(description string) {
-	tasks = append(tasks, Task{Description: description, Finished: false})
+func (tl *TaskList) printTask(task Task, number int, status string) {
+    fmt.Printf("%-5d %-10s %-10v %s\n", number, status, task.WorkTime.Truncate(time.Second), task.Description)
 }
 
-func removeTask(index int) {
-	if index < 1 || index > len(tasks) {
-		fmt.Println("Invalid task number")
-		return
-	}
-
-	tasks = append(tasks[:index-1], tasks[index:]...)
+func (tl *TaskList) addTask(description string) {
+    tl.Tasks = append(tl.Tasks, Task{Description: description, Finished: false})
 }
 
-func updateTask(index int, description string) {
-	if index < 1 || index > len(tasks) {
-		fmt.Println("Invalid task number")
-		return
-	}
+func (tl *TaskList) removeTask(index int) {
+    if index < 1 || index > len(tl.Tasks) {
+        fmt.Println("Invalid task number")
+        return
+    }
 
-	tasks[index-1].Description = description
+    tl.Tasks = append(tl.Tasks[:index-1], tl.Tasks[index:]...)
 }
 
-func finishTask(index int) {
-	if index < 1 || index > len(tasks) {
-		fmt.Println("Invalid task number")
-		return
-	}
+func (tl *TaskList) updateTask(index int, description string) {
+    if index < 1 || index > len(tl.Tasks) {
+        fmt.Println("Invalid task number")
+        return
+    }
 
-	tasks[index-1].Finished = true
+    tl.Tasks[index-1].Description = description
 }
 
-func workOnTask(index int) {
-	if index < 1 || index > len(tasks) {
-		fmt.Println("Invalid task number")
-		return
-	}
+func (tl *TaskList) finishTask(index int) {
+    if index < 1 || index > len(tl.Tasks) {
+        fmt.Println("Invalid task number")
+        return
+    }
 
-	start := time.Now()
-
-	fmt.Println("Press enter to stop working on the task...")
-	reader := bufio.NewReader(os.Stdin)
-	_, _ = reader.ReadString('\n')
-
-	tasks[index-1].WorkTime += time.Since(start)
+    tl.Tasks[index-1].Finished = true
 }
 
-func handleCommand(input string) {
-	tokens := strings.Split(input, " ")
-	command := tokens[0]
+func (tl *TaskList) workOnTask(index int) {
+    if index < 1 || index > len(tl.Tasks) {
+        fmt.Println("Invalid task number")
+        return
+    }
 
-	switch command {
-	case "add":
-		addTask(strings.Join(tokens[1:], " "))
-	case "remove":
-		removeTask(atoi(tokens[1]))
-	case "update":
-		updateTask(atoi(tokens[1]), strings.Join(tokens[2:], " "))
-	case "finish":
-		finishTask(atoi(tokens[1]))
-	case "work":
-		workOnTask(atoi(tokens[1]))
-	default:
-		fmt.Println("Invalid command")
-	}
+    start := time.Now()
+
+    fmt.Println("Press enter to stop working on the task...")
+    reader := bufio.NewReader(os.Stdin)
+    _, _ = reader.ReadString('\n')
+
+    tl.Tasks[index-1].WorkTime += time.Since(start)
 }
 
 func atoi(str string) int {
-	result, err := strconv.Atoi(str)
-	if err != nil {
-		return 0
-	}
-	return result
+    result, err := strconv.Atoi(str)
+    if err != nil {
+        return 0
+    }
+    return result
 }
-
-func saveTasksToFile() {
-	taskData, err := json.Marshal(tasks)
-	if err != nil {
-		fmt.Println("Error saving tasks to file:", err)
-		return
-	}
-
-	err = ioutil.WriteFile(taskFile, taskData, 0644)
-	if err != nil {
-		fmt.Println("Error saving tasks to file:", err)
-	}
-}
-
-func loadTasksFromFile() {
-	taskData, err := ioutil.ReadFile(taskFile)
-	if err != nil {
-		fmt.Println("No existing task file found. Starting with an empty task list.")
-		return
-	}
-
-	err = json.Unmarshal(taskData, &tasks)
-	if err != nil {
-		fmt.Println("Error loading tasks from file:", err)
-	}
-}
-
-var taskFile string
 
 func main() {
-	if len(os.Args) > 1 {
-		taskFile = os.Args[1]
-	} else {
-		taskFile = "tasks.dat"
-	}
+    taskFile := "tasks.dat"
+    if len(os.Args) > 1 {
+        taskFile = os.Args[1]
+    }
+    
+    taskList := &TaskList{}
+    taskList.loadTasksFromFile(taskFile)
 
-	loadTasksFromFile()
+    reader := bufio.NewReader(os.Stdin)
 
-	reader := bufio.NewReader(os.Stdin)
+    for {
+        taskList.printTasks()
 
-	for {
-		printTasks()
+        fmt.Print("> ")
+        input, _ := reader.ReadString('\n')
+        input = strings.TrimSpace(input)
 
-		fmt.Print("> ")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
+        if input == "quit" {
+            break
+        }
 
-		if input == "quit" {
-			break
-		}
+        taskList.handleCommand(input)
+    }
 
-		handleCommand(input)
-	}
+    taskList.saveTasksToFile(taskFile)
 
-	saveTasksToFile()
-
-	fmt.Println("Task list saved to file. Goodbye!")
+    fmt.Println("Task list saved to file. Goodbye!")
 }
+```
